c1d9438c9e900745a17b739ba97864ae79ca27d7
Amir1591 <60984539+Amir1591@users.noreply.github.com>
2020-02-12 19:46:37 -0500

    Initial commit

---
+++ app/Http/Controllers/PruebaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LaraTest\Http\Controllers;
+
+use LaraTest\Http\Controllers\Controller;
+
+class PruebaController extends Controller{
+	public function prueba(){
+		return 'Estoy dentro de PruebaController';
+	}
+
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LaraTest\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        // 'LaraTest\Model' => 'LaraTest\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
